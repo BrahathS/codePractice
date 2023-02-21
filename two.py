c570bc13406c7e80e1c5eb6f4d65d42811a86da7
Brahath Shet <bshet001@gold.ac.uk>
2023-02-21 15:23:35 +0000

    updated

--- two.py
+++ two.py
@@ -1,6 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        num_dict: Dict[int, int] = {}
+        for i, num in enumerate(nums):
+            # if target - num in num_dict, we've found a match
+            # Return the indices of the match and the current index
+            if target - num in num_dict:
+                return [num_dict[target - num], i]
+            # If no match, add the current number to the dictionary
+            # with the index as the value
+            num_dict[num] = i
+        # If no match is found, return an empty list
+        return []
